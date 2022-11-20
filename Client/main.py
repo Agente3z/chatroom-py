7d4a17a983cf079796c821019065d963ac345e2b
Christian Fiore <agente3z@yahoo.com>
2022-11-21 00:18:25 +0100

    quick cleanup

--- Client/main.py
+++ Client/main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from time import sleep
 
 serverUrl = "http://127.0.0.1:5000"
 
@@ -14,7 +15,6 @@ while True:
         username = input('Enter username: ')
         password = input('Enter password: ')
         r = requests.post(serverUrl, data={'method': 'register', 'username': username, 'password': password})
-        print(r.text)
     elif choice == '2':
         username = input('Enter username: ')
         password = input('Enter password: ')
@@ -22,6 +22,7 @@ while True:
         r = r.text
         if r == 'Login failed':
             print('Login failed')
+            sleep(2)
         else:
             with open('cookie.txt', 'w') as f:
                 f.write(r)
@@ -30,12 +31,10 @@ while True:
                 os.system('cls' if os.name == 'nt' else 'clear')
                 r = requests.post(serverUrl, data={'method': 'get'})
                 r = r.text
-                print(r)
                 msg = input('Message: ')
                 with open('cookie.txt', 'r') as f:
                     cookie = f.read()
                 r = requests.post(serverUrl, data={'method': 'send', 'cookie': cookie, 'msg': msg, 'username': username})
-                print(r.text)
 
     elif choice == '3':
         break
